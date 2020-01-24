06add8b42716ecebc24a23476c105bdcd2009ac1
todo2001 <todinimattia2001@gmail.com>
2020-01-24 10:17:36 +0100

    nomefile

---
+++ src/stikeball/Stikeball.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package stikeball;
+
+/**
+ *
+ * @author todin
+ */
+public class Stikeball {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
